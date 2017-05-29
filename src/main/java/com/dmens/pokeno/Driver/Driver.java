312eac621058ee09675912208591a9f51d05aa98
JingJunYin <jingjunyin@jingjuns-mbp.wireless.concordia.ca>
2017-05-29 18:40:10 -0400

    multiple changes in Driver (WIP)
    - added deck validation (WIP)
    - added const values for file locations
    - added a rough order of the game

--- src/main/java/com/dmens/pokeno/Driver/Driver.java
+++ src/main/java/com/dmens/pokeno/Driver/Driver.java
@@ -2,27 +2,147 @@ package com.dmens.pokeno.Driver;
 
 import com.dmens.pokeno.Card.*;
 import com.dmens.pokeno.Utils.*;
+import com.dmens.pokeno.Player.*;
+
 import java.util.*;
 
 public class Driver {
 
+	private static final String BASE_DIRECTORY = System.getProperty("user.dir");
+	private static final String LOCATION_FIRST_DECK = BASE_DIRECTORY + "/data/deck1.txt";
+	private static final String LOCATION_SECOND_DECK = BASE_DIRECTORY + "/data/deck2.txt";
+	private static final String LOCATION_CARDS = BASE_DIRECTORY + "/data/cards.txt";
+	private static final String LOCATION_ABILITIES = BASE_DIRECTORY + "/data/abilities.txt";
+
+	private static ArrayList<Card> mFirstDeck = null;
+	private static ArrayList<Card> mSecondDeck = null;
+	
+	private static ArrayList<Player> mPlayers = null;
+	
+	private static boolean mGameOver = false;
+		
 	public static void main(String[] args) {
-
-		System.out.println("Pokeno Parse Testing...");
 		
-		String directory = System.getProperty("user.dir"); 
-		String location1 = directory + "/data/deck1.txt";
-		String location2 = directory + "/data/deck2.txt";
-		String locationCards = directory + "/data/cards.txt";
-		String locationAbilities = directory + "/data/abilities.txt";
-
-		Parser.Instance().LoadCards(locationCards);
-		Parser.Instance().LoadAbilities(locationAbilities);
+		// Parse Cards
+		System.out.println("Parsing cards and abilities...");
+		boolean result = Parser.Instance().LoadCards(LOCATION_CARDS);
+		assert result;
+		result = Parser.Instance().LoadAbilities(LOCATION_ABILITIES);
+		assert result;
 		
-		ArrayList<Card> deck1 = Parser.Instance().DeckCreation(location1);
-		ArrayList<Card> deck2 = Parser.Instance().DeckCreation(location2);
-
+		// Deck creation and validation
+		System.out.println("Creating decks and validating them...");
+		mFirstDeck = Parser.Instance().DeckCreation(LOCATION_FIRST_DECK);
+		if(!IsDeckValid(mFirstDeck)) {
+			System.out.println("Deck is invalid");
+			// TODO: how do we handle invalid deck?
+		}
+		mSecondDeck = Parser.Instance().DeckCreation(LOCATION_SECOND_DECK);
+		if(!IsDeckValid(mSecondDeck)) {
+			System.out.println("Deck is invalid");
+			// TODO: how do we handle invalid deck?
+		}
 		
+		// Create Players and assign decks
+		// TODO: we need to allow player to choose his deck or randomly assign decks
+		System.out.println("Creating players and assigning decks...");
+		mPlayers = new ArrayList<Player>();
+		mPlayers.add(new Player(mFirstDeck));
+		mPlayers.add(new AIPlayer(mSecondDeck));
+
+		// Set up player's hand and rewards
+		for(int i = 0; i < mPlayers.size(); ++i) {
+			Player currentPlayer = mPlayers.get(i);
+			currentPlayer.drawCardsFromDeck(6);
+			//TODO: validate cards in hand for Mulligans here
+			//TODO: do we draw cards first or set up the rewards deck first?
+			currentPlayer.setUpRewards();
+		}
+		
+		// Main game loop
+		/*
+ 		while(!mGameOver) {
+			// TODO: each player plays his cards
+			// TODO: decide whether there is a winner
+		}
+		*/
+		
+		//Clean up	
 	}
-
+	
+	// Deck validation
+	public static boolean IsDeckValid(ArrayList<Card> deck) {
+			
+		if(deck == null)
+			return false;
+		// deck should have exactly 60 cards
+		if(deck.size() != 60)
+			return false;
+		// deck should have at least one pokemon card
+		if(!deckHasPokemonCard(deck)) 
+			return false;
+		if(deckHasMoreThanFourNoneEnergyCard(deck))
+			return false;
+		return true;
+	}
+	
+	private static boolean deckHasPokemonCard(ArrayList<Card> deck) {
+		assert deck != null;
+		if(deck.size() == 0) 
+			return false;
+		for(int i = 0; i < deck.size(); ++i) {
+			Card card = deck.get(i);
+			if(card instanceof Pokemon) {
+				return true;
+			}
+		}
+			
+		return false;
+	}
+	
+	private static boolean deckHasMoreThanFourNoneEnergyCard(ArrayList<Card> deck) {
+		
+		ArrayList<Card> deckWithNonEnergyCards = new ArrayList<Card>();
+		for(int i = 0; i < deck.size(); ++i) {
+			if(!(deck.get(i) instanceof EnergyCard)) {
+				deckWithNonEnergyCards.add(deck.get(i));
+			}
+		}
+		System.out.println("deckWithNonEnergyCards size is " + deckWithNonEnergyCards.size());
+		
+		//if(deckWithNonEnergyCards.get(0) instanceof myClass) {
+			
+//		}
+		for(int i = 0; i < deckWithNonEnergyCards.size(); ++i) {
+			Card currentCard = deckWithNonEnergyCards.get(i);
+			TrainerCard trainerCard = null;
+			Pokemon pokemon = null;
+			if(currentCard instanceof TrainerCard)
+				trainerCard = (TrainerCard)currentCard;
+			else if(currentCard instanceof Pokemon)
+				pokemon = (Pokemon)currentCard;
+			
+			if(trainerCard != null) {
+				for(int j = i + 1; j < deckWithNonEnergyCards.size(); ++j) {
+					if(deckWithNonEnergyCards.get(j) instanceof TrainerCard) {
+						//if(((TrainerCard)deckWithNonEnergyCards.get(j)).g)
+					}
+				}
+			}
+			Class currentClass = deckWithNonEnergyCards.get(i).getClass();
+			int cardCount = 1;
+			
+		}
+		
+		deckWithNonEnergyCards.get(0).getClass();
+     
+		return false;
+	}
+	
+	private static boolean allCardsArePlayableInDeck(ArrayList<Card> deck) {
+		
+		// TODO
+		
+		return true;
+	}
 }
